abb66dcd1d9782099d16f9e124dbc6a31942978e
04JoanaSofia <47534020+04JoanaSofia@users.noreply.github.com>
2021-01-10 15:44:01 +0000

    Add files via upload

--- boxSettings.js
+++ boxSettings.js
@@ -1,22 +1,97 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet,TouchableOpacity, Image, ScrollView} from 'react-native';
-import ButtonBlue  from '../components/button';
 import LogoSmall from '../components/logoSmall';
 import Navbar from '../components/navbar';
 import SettingsBox from '../components/settingsBox';
 import PeriodPicker from '../components/periodPicker';
+import ButtonBlue from '../components/button';
 
 export default function boxSettings ({navigation}) {
+
+    const [editablePeriod, setEditablePeriod] = useState(true);
+
+    const [isLoading, setLoading] = useState(true);
+    const [user, setUser] = useState('');
+    const [response, setResponse] = useState('');
+
+    let [sample, setSample] = useState();
+
+    let [username, setUsername] = useState('');
+    let [email, setEmail] = useState('');
+    let [sync, setSync] = useState();
+    let [shutdown, setShutdown] = useState();
+    let [firmware, setFirmware] = useState();
+
+    let samplePeriod;
+    let aux;
+
+    
+    useEffect(() => {
+        fetch('http://smartsensorbox.ddns.net:5000/usersettings/1')
+        .then((response) => response.json())
+        .then((json) => setUser(json[0]))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+
+    }, [global.editable]);
+
+    const _editPeriod=() => {
+        setEditablePeriod(false);
+    }
+
+    const saveChanges=() => {
+
+        username = user.username;
+        email = user.email;
+        sync = user.sync_period;
+        shutdown = user.shutdown_on_wakeup;
+        firmware = user.latest_firmware;
+
+        aux = sample;
+        samplePeriod = parseInt(aux);
+
+        const data = {
+            method: 'PUT',
+            headers: { 
+                'accept': 'application/json',
+                'Content-Type': 'application/json' },
+            body: JSON.stringify(
+                {
+                    "sync_period": sync,
+                    "sample_time": samplePeriod,
+                    "shutdown_on_wakeup": shutdown,
+                    "username": ''+username+'',
+                    "latest_firmware": firmware,
+                    "email": ''+email+''
+                  })
+                
+        };
+
+        console.log('entrou no savechages')
+        console.log('o que está dentro do header: '+data.headers)
+        console.log('o que está dentro do data: '+data.body)
+        fetch('http://smartsensorbox.ddns.net:5000/usersettings/1', data)
+        .then((response) => response.text())
+        .then((json) => setResponse(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+
+        console.log('atualizou?? '+response);
+
+        setEditablePeriod(true);
+   
+    }
+
     return(
-        
+ 
         <View style={styles.container}>
             <LogoSmall/>
             
             <Text style={styles.text}> Box name: </Text>
             <View style={styles.setting}>
-                <SettingsBox/>
-                <TouchableOpacity onPress={() => navigation.navigate('boxSettings')}>
+                <SettingsBox valueFromApi={'sensorBox 1'}/>
+                <TouchableOpacity>
                     <Image
                         source={require('../images/edit.png')}
                         style={styles.icon}/>
@@ -26,31 +101,36 @@ export default function boxSettings ({navigation}) {
             <Text style={styles.text}> Instalation date: </Text>
             <View style={styles.setting}>
                 <SettingsBox/>
-                <TouchableOpacity onPress={() => navigation.navigate('boxSettings')}>
+                <TouchableOpacity>
                     <Image
                         source={require('../images/edit.png')}
                         style={styles.icon}/>
                 </TouchableOpacity>
             </View>
+
 
             <Text style={styles.text}> Sample period: </Text>
             <View style={styles.setting}>
-                <PeriodPicker/>
-                <TouchableOpacity onPress={() => navigation.navigate('boxSettings')}>
+                <PeriodPicker
+                    canEdit={editablePeriod}
+                    samplePeriod={user.sample_time}
+                    onChangeItem={(item)=>setSample(item)}/>
+                <TouchableOpacity onPress={_editPeriod}>
                     <Image
                         source={require('../images/edit.png')}
                         style={styles.icon}/>
                 </TouchableOpacity>
             </View>
 
-           {/* <ButtonBlue 
-                style={styles.button}>
-                text='Reset'
-                onPress ={() => navigation.navigate('boxSettings')}>
-           </ButtonBlue>*/}
+           <ButtonBlue 
+                style={styles.button}
+                text='Save'
+                onPress={saveChanges}>
+           </ButtonBlue>
 
             <Navbar/>
         </View>
+
     );
 }
 
--- userSettings.js
+++ userSettings.js
@@ -1,14 +1,103 @@
 import 'react-native-gesture-handler';
-import React, {useState, setState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
 import SettingsBox  from '../components/settingsBox';
+import ButtonBlue from '../components/button';
 import LogoSmall from '../components/logoSmall';
 import Navbar from '../components/navbar';
 import UnitPicker from '../components/unitPicker';
 
 //import { GestureHandlerRootView } from 'react-native-gesture-handler';
+//const aux = true;
 
 export default function userSettings ({navigation}) {
+
+    const [editableUser, setEditableUser] = useState(false);
+    const [editableEmail, setEditableEmail] = useState(false);
+
+    const [isLoading, setLoading] = useState(true);
+    const [user, setUser] = useState('');
+    const [response, setResponse] = useState('');
+
+    let [username, setUsername] = useState('');
+    let [email, setEmail] = useState('');
+
+    let [sync, setSync] = useState();
+    let [sample, setSample] = useState();
+    let [shutdown, setShutdown] = useState();
+    let [firmware, setFirmware] = useState();
+
+    
+    useEffect(() => {
+
+        fetch('http://smartsensorbox.ddns.net:5000/usersettings/1')
+        .then((response) => response.json())
+        .then((json) => setUser(json[0]))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+
+    }, [global.editable]);
+
+    const _editUser=() => {
+        setEditableUser(true);
+    }
+
+    const _editEmail=() => {
+        setEditableEmail(true);
+    }
+
+    const saveChanges=() => {
+
+        if (username == null) {
+            username = user.username;
+        }
+        if (email == '') {
+            console.log('esta merda entra aqui');
+            email = user.email;
+            console.log('Qual é o valor do email? '+email)
+        }
+
+        sync = user.sync_period;
+        sample = user.sample_time;
+        shutdown = user.shutdown_on_wakeup;
+        firmware = user.latest_firmware;
+
+        const data = {
+            method: 'PUT',
+            headers: { 
+                'accept': 'application/json',
+                'Content-Type': 'application/json' },
+            body: JSON.stringify(
+                {
+                    "sync_period": sync,
+                    "sample_time": sample,
+                    "shutdown_on_wakeup": shutdown,
+                    "username": ''+username+'',
+                    "latest_firmware": firmware,
+                    "email": ''+email+''
+                  })
+                
+        };
+
+        console.log('entrou no savechages')
+        console.log('o que está dentro do header: '+data.headers)
+        console.log('o que está dentro do data: '+data.body)
+        fetch('http://smartsensorbox.ddns.net:5000/usersettings/1', data)
+        .then((response) => response.text())
+        .then((json) => setResponse(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+
+        console.log('atualizou?? '+response);
+
+        setEditableUser(false);
+        setEditableEmail(false);
+        console.log('editable user: '+editableUser);
+        console.log('editable email: '+editableEmail);
+   
+    }
+    
+
     return(
         <View style={styles.container}>
             <LogoSmall/>
@@ -16,8 +105,12 @@ export default function userSettings ({navigation}) {
            
             <Text style={styles.text}> Name: </Text>
             <View style={styles.setting}>
-                <SettingsBox/>
-                <TouchableOpacity onPress={() => navigation.navigate('userSettings')}>
+                <SettingsBox
+                    text={username}
+                    valueFromApi={user.username}
+                    onChangeText={(val)=>setUsername(val)}
+                    editable={editableUser}/>
+                <TouchableOpacity onPress={_editUser}>
                     <Image
                         source={require('../images/edit.png')}
                         style={styles.icon}/>
@@ -26,8 +119,12 @@ export default function userSettings ({navigation}) {
 
             <Text style={styles.text}> Email Address: </Text>
             <View style={styles.setting}>
-                <SettingsBox/>
-                <TouchableOpacity onPress={() => navigation.navigate('userSettings')}>
+                <SettingsBox
+                    text={email}
+                    valueFromApi={user.email}
+                    onChangeText={(val)=>setEmail(val)}
+                    editable={editableEmail}/>
+                <TouchableOpacity onPress={_editEmail}>
                     <Image
                         source={require('../images/edit.png')}
                         style={styles.icon}/>
@@ -37,7 +134,7 @@ export default function userSettings ({navigation}) {
             <Text style={styles.text}> Unit System: </Text>
             <View style={styles.setting}>
                 <UnitPicker/>
-                <TouchableOpacity onPress={() => navigation.navigate('userSettings')}>
+                <TouchableOpacity onPress={() => navigation.navigate('UserSettings')}>
                     <Image
                         source={require('../images/edit.png')}
                         style={styles.icon}/>
@@ -47,13 +144,18 @@ export default function userSettings ({navigation}) {
             <Text style={styles.text}> Boxes: </Text>
             <View style={styles.setting}>
                 <SettingsBox
-                    text='Smart Sensor Box 1'/>
-                <TouchableOpacity onPress={() => navigation.navigate('userSettings')}>
+                    valueFromAppi='Smart Sensor Box 1'/>
+                <TouchableOpacity>
                     <Image
                         source={require('../images/delete.png')}
                         style={styles.icon}/>
                 </TouchableOpacity>
             </View>
+
+            <ButtonBlue 
+                text='Save'
+                onPress={saveChanges}>
+           </ButtonBlue>
 
             <Image
                 source={require('../images/logout.png')}
@@ -98,3 +200,5 @@ const styles = StyleSheet.create({
 
     
 })
+
+//"{\n \"sync_period\": 10, \n \"sample_time\": 5, \n \"shutdown_on_wakeup\": true, \n \"username\":"+username+", \n \"latest_firmware\": 0.2, \n \"email\":"+user.email+" \"}")
