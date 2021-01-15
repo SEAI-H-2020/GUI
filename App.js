7ed95a335b67d978080b14a75bed1f350b0df19c
vascogradim <47787961+vascogradim@users.noreply.github.com>
2021-01-15 00:28:33 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -13,14 +13,32 @@ import reportProblems from './pages/report';
 import boxSettings from './pages/boxSettings';
 import userSettings from './pages/userSettings';
 import tempPage from './pages/temp';
-
+import humPage from './pages/hum'
+import noisePage from './pages/noise'
+import windPage from './pages/wind'
 
 
 const Stack = createStackNavigator();
 global.unitSystem = 'Metric';
+/**Temperatura */
 global.valores = [];
-global.valores[0]=15;
+global.valores[0] = 0;
 global.datas = [];
+
+/**Humidade */
+global.valoresHum = [];
+global.valoresHum[0] = 0;
+global.datasHum = [];
+
+/**Noise */
+global.valoresNoise = [];
+global.valoresNoise[0] = 0;
+global.datasNoise = [];
+
+/**Wind */
+global.valoresWind = [];
+global.valoresWind[0] = 0;
+global.datasWind = [];
 
 const App = () => {
   return (
@@ -28,12 +46,16 @@ const App = () => {
       <Stack.Navigator initialRouteName="login">
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="Profile" component={Register} />
-        <Stack.Screen name="chooseBox" component = {chooseBox} />
-        <Stack.Screen name="mainPage" component = {main} />
+        <Stack.Screen name="chooseBox" component={chooseBox} />
+        <Stack.Screen name="mainPage" component={main} />
         <Stack.Screen name="ReportProblems" component={reportProblems} />
         <Stack.Screen name="BoxSettings" component={boxSettings} />
         <Stack.Screen name="UserSettings" component={userSettings} />
         <Stack.Screen name="tempPage" component={tempPage} />
+        <Stack.Screen name="humPage" component={humPage} />
+        <Stack.Screen name="noisePage" component={noisePage} />
+        <Stack.Screen name="windPage" component={windPage} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
